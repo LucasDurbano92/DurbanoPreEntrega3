41e01ebd4eefba33f0f13966499a0628d60ca647
Lucas Durbano <lucas.nc.durbano@gmail.com>
2024-05-12 20:27:28 -0300

    creacion de modelo

--- LOFT/clase/models.py
+++ LOFT/clase/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=255)
+        
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+
+class Venta(models.Model):
+    Nro_Venta = models.PositiveIntegerField(unique=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True, blank=True)
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.SET_NULL, null=True, blank=True)
+    producto = models.ManyToManyField(Producto)
